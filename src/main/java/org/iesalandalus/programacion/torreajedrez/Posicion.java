89911bcd845727ef5f3ad5b00ec283d6de07d8af
ruben <ruben@DESKTOP-JO9KI7I>
2021-11-09 23:59:07 +0100

    Ejercicio 6

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -3,4 +3,26 @@ package org.iesalandalus.programacion.torreajedrez;
 public class Posicion {
 	private int fila;
 	private char columna;
+	public int getFila() {
+		return fila;
+	}
+	public void setFila(int fila) {
+		if (fila >= 1 || fila <= 8) { 
+			throw new IllegalArgumentException ("ERROR: La fila tiene que ser mayor que 0 y menor que 9"); 
+		}else {
+			this.fila = fila;
+		}
+	}
+	public char getColumna() {
+		return columna;
+	}
+	public void setColumna(char columna) {
+		if (columna >= 'a' || columna <= 'h') { 
+			throw new IllegalArgumentException ("ERROR: La columna tiene que ser estar entre la a y h"); 
+		}else {
+			this.columna = columna;
+
+	}
+}
+
 }
