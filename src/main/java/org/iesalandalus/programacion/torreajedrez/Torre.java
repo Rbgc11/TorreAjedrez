d332c93a57d7b4682b61d253fb72aba13fb4fe8c
ruben <ruben@DESKTOP-JO9KI7I>
2021-11-29 14:00:30 +0100

    Ejercicio 12

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -2,8 +2,22 @@ package org.iesalandalus.programacion.torreajedrez;
 
 public class Torre {
 	//Atributos
-		private Color color;
-		private Posicion posicion;
+	private Color color;
+	private Posicion posicion;
+		
+	//Constructores
+	public Color getColor() {
+	return color;
 	}
+	public void setColor(Color color) {
+	this.color = color;
+	}
+	public Posicion getPosicion() {
+	return posicion;
+	}
+	public void setPosicion(Posicion posicion) {
+	this.posicion = posicion;
+	}
+}
 
-
+	
