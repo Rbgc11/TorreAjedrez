920a36fb09344a9a89dd855f65145bcf51a4dce1
ruben <ruben@DESKTOP-JO9KI7I>
2021-11-29 22:40:07 +0100

    Ejercicio 20.13

--- src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
@@ -9,7 +9,24 @@ public class MainApp {
 	private static Torre torre;
 
 
+	public static void main(String[] args) {
+		int control=1;
+		int seleccion;
+		System.out.println("=====================================");
+		System.out.println("Torre Ajedrez");
+		System.out.println("=====================================");
 
+		do {
+			mostrarMenu();
+			seleccion=elegirOpcion();
+			ejecutarOpcion(seleccion);
+			mostrarTorre();
+			
+			if (seleccion==5) {
+				control=0;
+			}
+		} while (control!=0);
+	}
 	
 	//Metodo mostrarTorre
 	private static void mostrarTorre() {
